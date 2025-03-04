de95adc81c77b19fe7651d9ac81cbfd5bb1ddd32
Sigit Purnomo <sigit@staff.uajy.ac.id>
2025-03-04 10:25:10 +0700

    Update

--- get_data_berita.py
+++ get_data_berita.py
@@ -1,18 +1,28 @@
 # Function to scrape Google search results
-from seleniumbase import Driver
 import time
 import pandas as pd
 import streamlit as st
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from newspaper import Article
 
-
-from requests_html import HTMLSession
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 def get_urls_from_google(query, publisher, num_results):
-    driver = Driver(uc=True, headless=True, incognito=True)  # Use Undetected Chrome
+    firefoxOptions = Options()
+    firefoxOptions.add_argument("--headless")
+    service = Service(GeckoDriverManager().install())
+    driver = webdriver.Firefox(
+        options=firefoxOptions,
+        service=service,
+    )
+
     driver.get("https://www.google.co.id")
 
     search_input = driver.find_element("name", "q")
