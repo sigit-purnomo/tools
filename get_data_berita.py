90f7da77b422e6b00473e12c58aa849b985e0482
Sigit Purnomo <sigit@staff.uajy.ac.id>
2025-03-04 10:41:06 +0700

    Update get_data_berita.py

--- get_data_berita.py
+++ get_data_berita.py
@@ -5,6 +5,10 @@ import streamlit as st
 from newspaper import Article
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
