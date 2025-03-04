d64e8770ce21f9f06620058f49bc100540f40a05
Sigit Purnomo <sigit@staff.uajy.ac.id>
2025-03-04 11:45:11 +0700

    Update get_data_berita.py

--- get_data_berita.py
+++ get_data_berita.py
@@ -10,8 +10,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+
 from bs4 import BeautifulSoup
 
 # Function to scrape Google search results
